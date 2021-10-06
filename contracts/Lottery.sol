27bb027f6c6b988c01e8c778f057ed50d6a6055e
DieKant <olmastroni.fedro2000@gmail.com>
2021-10-06 21:35:32 +0200

    aggiunta algoritmo della funzione endtLottery, impostazione contratto VRF per l acquisizione di un numero random da parte dell oracle chainlink

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,14 +1,18 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.6.6;
-
+// prendo il contratto che mi da il valore eth/usd
 import "@chainlink/contracts/src/v0.6/interfaces/AggregatorV3Interface.sol";
+// prendo il contratto che mi permette di chiamare end e start lottery
 import "@openzeppelin/contracts/access/Ownable.sol";
+// prendo il contratto che mi permette di generare un numero random
+import "@chainlink/contracts/src/v0.6/VRFConsumerBase.sol";
 
-contract Lottery is Ownable {
+contract Lottery is VRFConsumerBase, Ownable {
     // un array di payable addresses che contiene i giocatori della lotteria
     address payable[] public players;
     uint256 public usdEntryFee;
+    uint256 public randomness;
     // variabile dove caricare il contratto dell'orcale chainlink
     AggregatorV3Interface internal ethUsdPriceFeed;
     // definisco gli stati della lotteria, anche se li scrivo a lettere è come se stessi scrivendo stato 0,1,2
@@ -18,12 +22,24 @@ contract Lottery is Ownable {
         CALCULATING_WINNER
     }
     LOTTERY_STATE public lottery_state;
+    bytes32 keyHash;
+    uint256 fee;
+    address public recentWinner;
 
-    constructor(address _priceFeedAddress) public {
+    // posso aggiungere i costruttori dei contratti dai quali inerito, qui gli do i valore del contratto VRF che mi prende il numero, il contratto link al quale pagare la fee, la fee e l'identificativo del contratto vrf sottoforma di keyhash
+    constructor(
+        address _priceFeedAddress,
+        address _vrfCoordinator,
+        address _link,
+        uint256 _fee,
+        bytes32 _keyHash
+    ) public VRFConsumerBase(_vrfCoordinator, _link) {
         usdEntryFee = 50 * (10**18);
         ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
         // quando deployo il contratto la lotteria è chiusa
         lottery_state = LOTTERY_STATE.CLOSE;
+        fee = _fee;
+        keyHash = _keyHash;
     }
 
     // la funzione per entrare nella lotteria
@@ -51,7 +67,7 @@ contract Lottery is Ownable {
         uint256 costToEnter = (usdEntryFee * 10**18) / adjustedPrice;
         return costToEnter;
     }
-    
+
     //solo l'autore può far partire o chiudere la lotteria
     function startLottery() public onlyOwner {
         // non avvio la lotteria se non è chiusa in primo luogo
@@ -63,6 +79,33 @@ contract Lottery is Ownable {
     }
 
     function endLottery() public onlyOwner {
-        
+        lottery_state = LOTTERY_STATE.CALCULATING_WINNER;
+        // questa funzione ritorna bytes32 requestId
+        requestRandomness(keyHash, fee);
+    }
+
+    // la rendo internal cosi solo il vrf può chaiamre questa funzione, ovverride è un opzione che sovrascrive il contenuto della funzione nel contratto d'appartenenza
+    function fulfillRandomness(bytes32 _requestId, uint256 _randomness)
+        internal
+        override
+    {
+        require(
+            lottery_state == LOTTERY_STATE.CALCULATING_WINNER,
+            "Non è ancora stato deciso il vincitore"
+        );
+        require(
+            _randomness > 0,
+            "il vrf è ubriaco e non ha ancora dato indietro il numero random"
+        );
+        // con questa operazione il modulo tornerà sempre un indice possibile per i posti dell'array players
+        uint256 indexOfWinner = _randomness % players.length;
+        recentWinner = players[indexOfWinner];
+        // trasferisco i big money dei poveri bastardi al fortunello maledetto
+        recentWinner.transfer(address(this).balance);
+        // resettiamo il tutto
+        // resetto i giocatori, lo stato della lotteria, il numero casuale
+        players = new address payable[](0);
+        lottery_state = LOTTERY_STATE.CLOSE;
+        randomness = _randomness;
     }
 }
